85a217e2d1067d7a853e6e432c3b565432324272
Lucis <luciannojunior@gmail.com>
2022-11-22 13:56:02 -0300

    Creating index page

--- routes/cards/index.tsx
+++ routes/cards/index.tsx
@@ -1,8 +1,50 @@
 import { Handlers, HandlerContext, PageProps } from "$fresh/server.ts";
+import type { UserResponse } from "supabase";
 import { createPrivateHandler, EjdcardState } from "../../auth.ts";
 
-type Operation = "credit" | "debit" | "read" | "manage";
-type PageData = { allowedOperations: Operation[] };
+type Operation = "credit" | "debit" | "read" | "manage" | "onboard";
+
+type PageData = { allowedOperations: Operation[]; user: UserResponse };
+
+const permissionLetterToOperation: Record<string, Operation> = {
+  m: "manage",
+  c: "credit",
+  d: "debit",
+  r: "read",
+  o: "onboard",
+};
+
+const operationPages: Record<Operation, { label: string; href: string }> = {
+  credit: {
+    label: "Recarga de Cartão",
+    href: "/cards/operation?op=credit",
+  },
+
+  debit: {
+    label: "Vendas",
+    href: "/cards/operation?op=debit",
+  },
+
+  onboard: {
+    label: "Cadastro de Cartão",
+    href: "/cards/new",
+  },
+
+  read: {
+    label: "Consulta de Saldo",
+    href: "/cards/query",
+  },
+  manage: {
+    label: "Administração",
+    href: "/cards/admin",
+  },
+};
+// TODO: Replace with automatic types from Supabase
+type Profile = {
+  id: string;
+  name: string;
+  permissions?: string;
+};
 
 export const handler = {
   GET: createPrivateHandler(
@@ -10,12 +52,44 @@ export const handler = {
     async (req: Request, ctx: HandlerContext<PageData, EjdcardState>) => {
       const client = ctx.state.client;
       const { data } = await client.from("profiles").select("*");
-      console.log({ data });
-      return ctx.render();
+      const myUserId = ctx.state.user.data.user?.id;
+      // TODO: Remove coersion
+      const myProfile = (data as Profile[]).find(({ id }) => id === myUserId);
+      if (!myProfile?.permissions) {
+        return ctx.render({ allowedOperations: [], user: ctx.state.user });
+      }
+
+      // Ex: 'vucrd'
+      const allowedOperations = myProfile.permissions
+        .split("")
+        .map((letter) => permissionLetterToOperation[letter]);
+
+      return ctx.render({ allowedOperations, user: ctx.state.user });
     }
   ),
 } as Handlers;
 
-export default function CardsIndex(props: PageProps<PageData>) {
-  return <div></div>;
+export default function CardsIndex({
+  data: {
+    allowedOperations,
+    user: { data: userData },
+  },
+}: PageProps<PageData>) {
+  const email = userData?.user?.email;
+  return (
+    <div class="p-4 flex flex-col">
+      <h1 class="text-2xl">{`Bem-vindx, ${email}`}</h1>
+      <span>Você tem acesso as seguintes páginas:</span>
+      <ul>
+        {allowedOperations.map((operation) => {
+          const { label, href } = operationPages[operation];
+          return (
+            <li class="pt-1">
+              <a href={href}>{label}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
--- routes/login/index.tsx
+++ routes/login/index.tsx
@@ -1,9 +1,66 @@
-import LoginButtons from "../../islands/LoginButtons.tsx";
+import LoginButtons from "$ejdcard/components/LoginButtons.tsx";
+import { Handler, PageProps } from "$fresh/server.ts";
+import { getSupabaseClientForUser } from "$ejdcard/auth.ts";
 
-export default function LoginRoute() {
+export const RETURN_URL_QUERY_PARAM = "returnUrl";
+
+export const DEFAULT_RETURN_URL = "/cards";
+
+type LoginData = {
+  loginLinks: {
+    google: string;
+  };
+};
+
+export const handler: Handler<LoginData> = async (req, ctx) => {
+  const url = new URL(req.url);
+  const query = new URLSearchParams(url.search);
+  const providedReturnUrl = query.get(RETURN_URL_QUERY_PARAM);
+  const returnUrl = providedReturnUrl || DEFAULT_RETURN_URL;
+
+  const successQueryParams = new URLSearchParams();
+  successQueryParams.set(RETURN_URL_QUERY_PARAM, returnUrl);
+
+  const redirectTo =
+    url.protocol +
+    "//" +
+    url.host +
+    "/login/success?" +
+    successQueryParams.toString();
+
+  const client = await getSupabaseClientForUser(req);
+
+  if (!client) {
+    return ctx.render({
+      loginLinks: {
+        google: "#",
+      },
+    });
+  }
+
+  const {
+    data: { url: googleLoginUrl },
+  } = await client.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo,
+    },
+  });
+
+  return ctx.render({
+    loginLinks: {
+      google: googleLoginUrl ?? "#",
+    },
+  });
+};
+export default function LoginRoute({
+  data: {
+    loginLinks: { google },
+  },
+}: PageProps<LoginData>) {
   return (
     <div>
-      <LoginButtons />
+      <LoginButtons googleLoginUrl={google} />
     </div>
   );
 }
